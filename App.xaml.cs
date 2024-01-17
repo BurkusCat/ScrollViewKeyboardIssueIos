c4465bc5853f5dc8ef6f9e52b1ec14960bd3e7cb
Ronan Burke <burkuscat@gmail.com>
2024-01-17 20:34:28 +0000

    Basic project

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ScrollViewKeyboardIssueIos
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ScrollViewKeyboardIssueIos
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
